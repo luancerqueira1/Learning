3411586594252deeb5c8aeacb515aefb006de122
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-06-18 16:45:19 -0300

    break time

--- Portfolio Website Tutorial – Frontend Development with HTML, CSS, JavaScript/js/app.js
+++ Portfolio Website Tutorial – Frontend Development with HTML, CSS, JavaScript/js/app.js
@@ -4,16 +4,48 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 
 
-function pageTransitions() {
-	//BUTTON CLICK ACTIVE CLASS
-	for (let i = 0; i < sectBtn.length; i++){
-		sectBtn[i].addEventListener('click', function() {
-			let currentBtn = document.querySelectorAll('.active-btn');
-			currentBtn[0].className = currentBtn[0].className.replace('.active-btn', '');
-			this.className += 'active-btn'
-		})
-	}
+/* NAO FUNCIONOU */
+// function pageTransitions() {
+// 	//BUTTON CLICK ACTIVE CLASS
+// 	for (let i = 0; i < sectBtn.length; i++){
+// 		sectBtn[i].addEventListener('click', function() {
+// 			let currentBtn = document.querySelectorAll('.active-btn');
+// 			currentBtn[0].className = currentBtn[0].className.replace('.active-btn', '');
+// 			this.className += 'active-btn'
+// 		})
+// 	}
+// }
+// pageTransitions();
 
+//NAVIGATION BUTTONS ACTIVE
+//OUTO QUE DEU CERTO
+function activeLink() {
+	sectBtn.forEach((item) => {
+		item.classList.remove('active-btn');
+		this.classList.add('active-btn');
+	});
+	
 }
+//sections active
+allSections.addEventListener('click', (e) => {
+	const id = e.target.dataset.id;
+	if (id) {
+		//remove selected from the other btns
+		sectBtns.forEach((btn) => {
+			btn.classList.remove('active')
+		})
+		e.target.classList.add('active')
+		//HIDE OTHER sections
+		sections.forEach((section) => {
+			section.classList.remove('active')
+		})
 
-pageTransitions();
+		const element = document.getElementById(id);
+		element.classList.add('active')
+	}
+});
+
+sectBtn.forEach((item) => {
+	item.addEventListener('click', activeLink);
+	
+})
